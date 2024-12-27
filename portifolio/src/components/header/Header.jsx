c80a080bb58205d7ade95176bd20f7058c2663d9
Vinícius Pereira Polli <viniciusppolli07@gmail.com>
2024-12-27 00:23:23 -0300

    Finalização do Header, e responsividade do header para celular

--- portifolio/src/components/header/Header.jsx
+++ portifolio/src/components/header/Header.jsx
@@ -1,55 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 
 export const Header = () => {
+  /*=============== Toggle Menu ===============*/
+  const[Toggle, showMenu] = useState(false);
+
   return (
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">Smith</a>
 
-        <div className="nav__menu">
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link">
-                <i className="uil uil-estate nav__icon"></i>
-              </a>Home
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
             </li>
 
             <li className="nav__item">
               <a href="#about" className="nav__link">
-                <i className="uil uil-user nav__icon"></i>
-              </a>About
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
             </li>
 
             <li className="nav__item">
               <a href="#skills" className="nav__link">
-                <i className="uil uil-file-alt nav__icon"></i>
-              </a>Skills
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a> 
             </li>
 
             <li className="nav__item">
               <a href="#services" className="nav__link">
-                <i className="uil uil-briefcase-alt nav__icon"></i>
-              </a>Services
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
             </li>
 
             <li className="nav__item">
               <a href="portifolio" className="nav__link">
-                <i className="uil uil-scenery nav__icon"></i>
-              </a>Portifólio
+                <i className="uil uil-scenery nav__icon"></i> Portifólio
+              </a>
             </li>
 
             <li className="nav__item">
               <a href="#contact" className="nav__link">
-                <i className="uil uil-message nav__icon"></i>
-              </a>Contact
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
             </li>
           </ul>
 
-          <i class="uil uil-times nav__close"></i>
+          <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
         </div>
 
-        <div className="nav__toggle">
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
           <i class="uil uil-apps"></i>
         </div>
       </nav>
